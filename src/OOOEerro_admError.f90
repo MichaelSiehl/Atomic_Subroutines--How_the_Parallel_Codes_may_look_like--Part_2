2dd83a11cbf0d02c7aefececd5f52791d8785eff
Michael Siehl <miesiehl@gmail.com>
2017-04-15 19:29:52 +0200

    Add files via upload

---
+++ src/OOOEerro_admError.f90
@@ -0,0 +1,66 @@
+! https://github.com/MichaelSiehl/Atomic_Subroutines--How_the_Parallel_Codes_may_look_like--Part_2
+
+module OOOEerro_admError
+
+use OOOGglob_Globals
+
+implicit none
+!___________________________________________________________
+!
+private
+!
+public :: OOOEerroc_AddObject
+!___________________________________________________________
+!
+type, public :: OOOEerroc_colError
+  private
+  !
+end type OOOEerroc_colError
+!___________________________________________________________
+
+contains
+!
+!___________________________________________________________
+!
+subroutine OOOEerroc_AddObject (Collection, chrErrorDescription, &
+  intErrorType)
+  type (OOOEerroc_colError), intent (inout) :: Collection
+  character(kind=OOOGglob_kcha,len=*), intent(in) :: chrErrorDescription
+  integer(kind=OOOGglob_kint), intent(in) :: intErrorType
+  character(kind=OOOGglob_kcha,len=OOOGglob_Len200) :: chrSelection
+  !
+  !
+write(*,*) "CurrentProc: ", OOOGglob_chrCurrentProcedure
+!
+write(*,*) "ErrDescr: ", trim(chrErrorDescription)
+!
+write(*,*)  "ErrTyp: ", intErrorType
+!
+write(*,*)
+write(*,*) "CallingProc: ", trim(OOOGglob_chrCallingProcedure)
+!
+write(*,*) "ReturningProc: ", trim(OOOGglob_chrReturningProcedure)
+!
+print *
+write(*,*) "     *** An error occured ! *** "
+print *, ' Please select: '
+print *
+print *, ' x  - Exit Program, or'
+print *, ' c  - Continue Execution'
+print *
+!
+print *, ' -> '
+read *, chrSelection
+print *
+chrSelection = trim(chrSelection)
+select case (chrSelection)
+  case ('x', 'X')
+  !
+      error stop ! end of execution
+  !
+end select
+!
+end subroutine OOOEerroc_AddObject
+!___________________________________________________________
+!
+end module OOOEerro_admError
---
+++ src/OOOGglob_Globals.f90
@@ -0,0 +1,105 @@
+! https://github.com/MichaelSiehl/Atomic_Subroutines--How_the_Parallel_Codes_may_look_like--Part_2
+
+module OOOGglob_Globals
+
+  implicit none
+  public
+  ! Application Main Directory Path:
+  character(kind=1,len=240) :: OOOGglob_chrAppPath = ""
+  !
+  ! upper bound declarations for the static components of
+  ! derived type coarrays:
+  integer, parameter :: OOOGglob_TeamManagers_UpperBound = 5 ! max number of TeamManagers
+  integer, parameter :: OOOGglob_TeamMembers_UpperBound = 10 ! max number of TeamMembers per team
+  integer, parameter :: OOOGglob_NumImages_UpperBound = 100 ! max number of images
+  !
+  !********************************************************
+  !********************************************************
+  ! Stack Trace:
+  character(kind=1,len=35), dimension (1:50) :: OOOGglob_chrStackTrace
+  integer(kind=4) :: OOOGglob_intStackTraceCounter = 0
+  !
+  !********************************************************
+  !********************************************************
+  ! kind values:
+  integer, parameter :: OOOGglob_kint = 4 ! kind for integer
+  integer, parameter :: OOOGglob_krea = 4 ! kind for real
+  integer, parameter :: OOOGglob_kcom = 4 ! kind for COMPLEX
+  integer, parameter :: OOOGglob_klog = 4 ! kind for logical
+  integer, parameter :: OOOGglob_kcha = 1 ! kind for character, 1=ASCII
+  integer, parameter :: OOOGglob_Len40 = 40 ! len for character
+  integer, parameter :: OOOGglob_Len200 = 200 ! len for character
+  integer, parameter :: OOOGglob_Len240 = 240 ! len for character
+  integer, parameter :: OOOGglob_Len2400 = 2400 ! len for character
+  !
+  !**********************************************************
+  ! Input/Output-constants
+  ! file UNITs:
+  integer(kind=4), parameter :: OOOGglob_FileUnitA = 1
+  integer(kind=4), parameter :: OOOGglob_FileUnitB = 2
+  ! for Tracing und Error-Handling:
+  integer(kind=4), parameter :: OOOGglob_TracingFileUnit = 3
+  integer(kind=4), parameter :: OOOGglob_StackTraceFileUnit = 4
+  integer(kind=4), parameter :: OOOGglob_ErrorLogFileUnit = 7 ! 5 is keyboard and 6 is screen
+  !
+  !**********************************************************
+  ! Error-Handling
+  character(kind=1,len=40) :: OOOGglob_chrCurrentProcedure = "Main"
+  character(kind=1,len=40) :: OOOGglob_chrCallingProcedure = "User"
+  character(kind=1,len=40) :: OOOGglob_chrReturningProcedure = ""
+  character(kind=1,len=40) :: OOOGglob_chrTempCallingProcedur = ""
+
+  !**********************************************************
+  ! Error-Handling
+  real(kind=4) :: OOOGglob_reaNaN
+  real(kind=4) :: OOOGglob_reaPositiveInfinity
+  real(kind=4) :: OOOGglob_reaNegativeInfinity
+
+  !**********************************************************
+  ! Error-Handling
+  integer(kind=4), parameter :: OOOGglob_warning = 1
+  integer(kind=4), parameter :: OOOGglob_error = 2
+  integer(kind=4), parameter :: OOOGglob_NoErrNumber = 0
+  !
+
+!**********************************************************
+contains
+
+! Public-Subroutines:
+!__________________________________________________________
+
+!***********************************************************
+!
+subroutine OOOGglob_subSetProcedures (chrCurrentProcedure)
+  ! Error-Handling and Tracing
+  character(kind=OOOGglob_kcha,len=*), intent(in) :: chrCurrentProcedure
+  ! refresh the Stack Trace:
+!  OOOGglob_intStackTraceCounter = OOOGglob_intStackTraceCounter + 1
+!  OOOGglob_chrStackTrace (OOOGglob_intStackTraceCounter) = chrCurrentProcedure
+!  OOOGglob_chrCurrentProcedure = OOOGglob_chrStackTrace (OOOGglob_intStackTraceCounter)
+!  OOOGglob_chrCallingProcedure = OOOGglob_chrStackTrace (OOOGglob_intStackTraceCounter - 1)
+!  OOOGglob_chrReturningProcedure = ''
+  !
+end subroutine OOOGglob_subSetProcedures
+!_____________
+!
+subroutine OOOGglob_subResetProcedures
+  ! Error-Handling and Tracing
+  ! refresh the Stack Trace:
+!  OOOGglob_intStackTraceCounter = OOOGglob_intStackTraceCounter - 1
+!  OOOGglob_chrCurrentProcedure = OOOGglob_chrStackTrace (OOOGglob_intStackTraceCounter)
+!  OOOGglob_chrReturningProcedure = OOOGglob_chrStackTrace (OOOGglob_intStackTraceCounter + 1)
+!  if (OOOGglob_intStackTraceCounter > 1) then
+!    OOOGglob_chrCallingProcedure = OOOGglob_chrStackTrace (OOOGglob_intStackTraceCounter - 1)
+!  end if
+  !
+end subroutine OOOGglob_subResetProcedures
+!__________________________________________________________
+!
+!************************************************************
+
+!**********************************************************
+!**********************************************************
+!**********************************************************
+
+end module OOOGglob_Globals
